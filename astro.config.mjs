de3bd0f1b9037b98698a5f9913d1f9fdf2129ba1
Deva Pramod <112617358+DevaP11@users.noreply.github.com>
2024-12-24 18:29:38 +0530

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,17 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [
-        mdx(),
-        sitemap(),
-        tailwind({
-            applyBaseStyles: false
-        })
-    ]
+  site: 'https://example.com',
+  integrations: [
+    mdx(),
+    sitemap(),
+    pagefind(),
+    tailwind({
+      applyBaseStyles: false
+    })
+  ]
 });
